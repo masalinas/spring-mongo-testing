fe51bfc5467caac4d82bd9eaaf99bafe4235c85b
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-10-25 19:31:23 +0200

    first commit

--- src/main/java/io/oferto/testing/TestingApplication.java
+++ src/main/java/io/oferto/testing/TestingApplication.java
@@ -1,39 +1,48 @@
 package io.oferto.testing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+
+import io.oferto.testing.property.OpenApiProperties;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+
 import lombok.Generated;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OpenApiProperties.class)
 public class TestingApplication {
 
-	@Value("${api.common.title}") String apiTitle;
+	/*@Value("${api.common.title}") String apiTitle;
 	@Value("${api.common.version}") String apiVersion;
 	@Value("${api.common.description}") String apiDescription;
 	@Value("${api.common.contact.name}") String apiContactName;
 	@Value("${api.common.contact.url}")  String apiContactUrl;
-	@Value("${api.common.contact.email}") String apiContactEmail;
+	@Value("${api.common.contact.email}") String apiContactEmail;*/
+	
+	@Autowired
+    private OpenApiProperties openApiProperties;
 	
 	@Bean
-	public OpenAPI getOpenApiDocumentation() {
+	public OpenAPI getOpenApiDocumentation() {		
 		return new OpenAPI()
-			.info(new Info().title(apiTitle).
-				description(apiDescription)
-		        .version(apiVersion)
-		        .contact(new Contact()
-		          .name(apiContactName)
-		          .url(apiContactUrl)
-		          .email(apiContactEmail)));
-	}
+					.info(new Info().title(openApiProperties.getCommon().getTitle()).
+						description(openApiProperties.getCommon().getDescription())
+				        .version(openApiProperties.getCommon().getVersion())
+				        .contact(new Contact()
+				          .name(openApiProperties.getCommon().getContact().getName())
+				          .url(openApiProperties.getCommon().getContact().getUrl())
+				          .email(openApiProperties.getCommon().getContact().getEmail())));		
+		}
 	
 	@Generated
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(TestingApplication.class, args);
 	}	
 }
---
+++ src/main/java/io/oferto/testing/property/OpenApiProperties.java
@@ -0,0 +1,26 @@
+package io.oferto.testing.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@ConfigurationProperties(prefix = "api")
+@Data
+public class OpenApiProperties {	
+	private Common common = new  Common();
+	
+	@Data
+	public static class Common {        
+		private String version;
+        private String title;
+        private String description;
+        private Contact contact = new Contact();
+            
+        @Data
+        public static class Contact {            
+			private String name;
+            private String url;
+            private String email;
+        }
+    }
+}
